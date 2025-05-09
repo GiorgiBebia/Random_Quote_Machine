973e24ead53845ef9568583dcca36b9591a8ac49
Giorgi Bebia <elogiogio@gmail.com>
2025-05-09 18:09:17 +0400

    Random Quote Machine

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,8 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        manrope: ["Manrope", "sans-serif"],
-      },
-    },
-    screens: {
-      tablet: { max: "768px" },
-      mobile: { max: "375px" },
-    },
+    extend: {},
+    screens: {},
   },
   plugins: [],
 };
